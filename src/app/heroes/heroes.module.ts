d8a849543a5e2e4ffa84906ec53ea3e56f606037
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-18 14:04:10 +0200

    Add hero search

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,11 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
     //pipes
     HeroImagePipe,
   ],
-  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
 export class HeroesModule {}
