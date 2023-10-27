8e6af4d3336d40bdaf683244dbba7733b39d3288
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-27 20:01:58 +0200

    Add dialog to confirm delation

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { environment } from 'src/environments/environments';
 
 @Injectable({
@@ -24,5 +24,21 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHero(id: string): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((error) => of(false)),
+      map((resp) => true)
+    );
   }
 }
