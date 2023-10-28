d472f5feedc807bd8ad3ca9b2b992c21b1a3d73b
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-10-28 20:28:12 +0200

    Finish mock auth

--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -8,34 +8,29 @@ import {
   RouterStateSnapshot,
   Router,
 } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanMatch, CanActivate {
+export class PublicGuard implements CanMatch, CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   private checkAuthStatus(): boolean | Observable<boolean> {
     return this.authService.checkAuthentication().pipe(
       tap((isAuthenticated) => console.log({ authenticated: isAuthenticated })),
       tap((isAuthenticated) => {
-        if (!isAuthenticated) {
-          this.router.navigate(['/auth/login']);
+        if (isAuthenticated) {
+          this.router.navigate(['/']);
         }
-      })
+      }),
+      map((isAuthenticated) => !isAuthenticated)
     );
   }
 
-  canMatch(
-    route: Route,
-    segments: UrlSegment[]
-  ): boolean | Observable<boolean> {
+  canMatch(): boolean | Observable<boolean> {
     return this.checkAuthStatus();
   }
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | Observable<boolean> {
+  canActivate(): boolean | Observable<boolean> {
     return this.checkAuthStatus();
   }
 }
